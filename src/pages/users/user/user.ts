6f3056b14fe72627fcc01cb0bb6069c2d03f617a
Himanshu Goyal <himanshu.goyal@live.com>
2018-08-25 15:01:37 +0530

    Mastering Basics: Passing Data Between Pages

--- src/pages/users/user/user.ts
+++ src/pages/users/user/user.ts
@@ -1,9 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { NavController, NavParams } from "ionic-angular";
 
 @Component({
     selector: 'page-user',
     templateUrl: 'user.html'
 })
-export class UserPage {
-    name: string;
+export class UserPage implements OnInit {
+    
+   name: string;
+    /**
+     * NavParams Allows to retreive the data
+     */
+    constructor(private navParams: NavParams) {
+   
+    }
+
+    ngOnInit() {
+        this.name = this.navParams.get('userName');
+    }
+   
 }
--- src/pages/users/users.ts
+++ src/pages/users/users.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { UserPage } from './user/user';
 
 @IonicPage()
 @Component({
@@ -7,5 +8,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'users.html',
 })
 export class UsersPage {
-name: string;
+
+/**
+ *
+ */
+constructor(private navCtrl: NavController) {
+  
+}
+onLoadUser(name: string){
+  //Note: Pushing the page on the stack and also adding some data
+  this.navCtrl.push(UserPage, {userName: name});
+}
 }
