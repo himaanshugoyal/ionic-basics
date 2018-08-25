bbbc24617310e0f1f86581f77bdcbaee2ec5c41e
Himanshu Goyal <himanshu.goyal@live.com>
2018-08-25 15:15:50 +0530

    Mastering Basics | Popping Pages going back

--- src/pages/users/user/user.ts
+++ src/pages/users/user/user.ts
@@ -11,12 +11,19 @@ export class UserPage implements OnInit {
     /**
      * NavParams Allows to retreive the data
      */
-    constructor(private navParams: NavParams) {
+    constructor(private navParams: NavParams, 
+    private navCtrl: NavController) {
    
     }
 
     ngOnInit() {
         this.name = this.navParams.get('userName');
+    }
+
+    onGoBack(){
+
+       // this.navCtrl.pop(); // this will pop the current page
+       this.navCtrl.popToRoot(); // this will pop and take to the root page
     }
    
 }
