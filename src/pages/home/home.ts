c174c3642f645889b57e7e6a6a34bf955bc4de98
Himanshu Goyal <himanshu.goyal@live.com>
2018-08-25 14:21:43 +0530

    Mastering Basics: Creating a Page and How to navigate to it

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { UsersPage } from '../users/users';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  //Nav Controller is used to manage the stack of pages
+  // Note that this importing from ionic-angular
   constructor(public navCtrl: NavController) {
 
+  }
+
+  onGoToUsers() {
+    this.navCtrl.push(UsersPage);
   }
 
 }
